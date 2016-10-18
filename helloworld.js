5cb1b49ed55b9150675bbea0802ee6761caaa3dc
Donna Ryan <donna@nerdrose.com>
2016-10-17 17:03:28 -0700

    Updated to ask who you are

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,4 @@
-console.log('Hello, World!');
+var who = prompt('What is your name?');
+var txtDisplay = 'Hello, ' + who + '!';
+console.log(txtDisplay);
+document.getElementById('hello').innerHTML = txtDisplay;
